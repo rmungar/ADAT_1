4fcfd566224a286f3c84918b4835e0e287e36c32
rmungar1209 <rmungar1209@g.educaand.es>
2024-09-25 23:32:38 +0200

    Terminado

--- src/main/kotlin/FileReader.kt
+++ src/main/kotlin/FileReader.kt
@@ -1,4 +1,53 @@
 package org.example
 
+import java.io.File
+
 class FileReader {
+
+
+
+    fun readFile(file: File): List<String>{
+        var contenido: List<String> = emptyList()
+        if (file.exists()){
+             contenido = file.useLines { it.toList() }
+        }
+        return contenido
+    }
+
+
+    fun extraerDatos(contenido: List<String>): MutableList<Map<String, List<String>>> {
+
+        var linea = 1
+        val datos = mutableListOf<Map<String,List<String>>>()
+        while (linea < contenido.size){
+            var cont = 0
+            val contenidoLinea = contenido[linea].split(";")
+            var nombreEmpresa = ""
+            val listaContabilidad = mutableListOf<String>()
+            while (cont < contenidoLinea.size){
+                if (cont == 0){
+                    nombreEmpresa = contenidoLinea[cont]
+                }
+                else{
+                    listaContabilidad.add(contenidoLinea[cont])
+                }
+                cont++
+            }
+            datos.add(mapOf(Pair(nombreEmpresa, listaContabilidad.toList())))
+            linea++
+        }
+
+        return datos
+    }
+
+
+
+    fun guardarDatos(fichero: File, datos: MutableList<Map<String, List<String>>> ){
+
+        datos.forEach {empresa ->
+            fichero.appendText("${empresa.keys};${empresa.values.toList()[0][1].replace(",",".").toDouble()};${empresa.values.toList()[0][2].replace(",",".").toDouble()};${(empresa.values.toList()[0][1].replace(",",".").toDouble())+(empresa.values.toList()[0][2].replace(",",".").toDouble())/2}\n")
+        }
+
+
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 package org.example
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+
+    val userDir = System.getProperty("user.dir")
+
+    val ficheroCotizacion = File("$userDir\\src\\main\\resources\\cotizacion.csv")
+
+    val lectorDeFicheros = FileReader()
+    val contenido = lectorDeFicheros.readFile(ficheroCotizacion)
+    val desglose = lectorDeFicheros.extraerDatos(contenido)
+    println(desglose)
+
+
+
+    val  nuevoFichero = File("$userDir\\src\\main\\resources\\cotizacion2.csv")
+    if (!nuevoFichero.exists()){
+        nuevoFichero.createNewFile()
+    }
+    lectorDeFicheros.guardarDatos(nuevoFichero, desglose)
 }
